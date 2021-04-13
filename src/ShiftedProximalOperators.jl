07f38cd459a089393c683671dfa80a48508a9fb8
rjbaraldi <rbaraldi@uw.edu>
2021-04-12 23:45:41 -0400

    shiftedl0norm changes b/c R was being difficult

--- src/ShiftedProximalOperators.jl
+++ src/ShiftedProximalOperators.jl
@@ -1,15 +1,15 @@
 module ShiftedProximalOperators
 
-using ProximalOperators, LinearAlgebra
+using ProximalOperators
 
 export PROXPATH
 PROXPATH = dirname(@__DIR__)
 
 abstract type ShiftedProximalFunction <: ProximableFunction end
 
-export prox, shift!
-
 # prox files 
 include("shiftedl0norm.jl")
+
+export prox, shift!
 
 end # module
--- src/shiftedl0norm.jl
+++ src/shiftedl0norm.jl
@@ -1,26 +1,25 @@
 export ShiftedNormL0
 
-mutable struct ShiftedNormL0{R <: Real} <: ProximableFunction
-	h
+mutable struct ShiftedNormL0 <: ShiftedProximalFunction
+	h::ProximableFunction
 	x
-	s
 	λ
-	function ShiftedNormL0(x::AbstractVector{R}, λ::R)
-		new(x, similar(x), λ)
+	function ShiftedNormL0(n, λ)
+		new(NormL0(λ), Vector{Float64}(undef, n), λ)
 	end
 end
 
-(ψ::ShiftedProximalFunction)(y) = ψ.h (ψ.x + y)
+(ψ::ShiftedProximalFunction)(y) = ψ.h(ψ.x + y)
 
-shifted(h::NormL0{R}, x::AbstractVector{R}, λ::R) = ShiftedNormL0{R}(h, x, λ)
+shifted(h::NormL0, x::Vector{Float64}, λ)= ShiftedNormL0(h, x, λ)
 
-function shift!(ψ::ShiftedNormL0{R}, x::AbstractVector{R})
+function shift!(ψ::ShiftedNormL0, x::Vector{Float64})
 	ψ.x .= x
 	ψ
 end
 
 function prox(ψ::ShiftedNormL0, q, σ)
-	c = sqrt(2 * σ)
+	c = sqrt(2 * ψ.λ * σ)
 	for i ∈ eachindex(q)
 	xpq = ψ.x[i] + q[i]
 		absxpq = abs(xpq)
@@ -33,3 +32,6 @@ function prox(ψ::ShiftedNormL0, q, σ)
 	ψ.s .-= ψ.x
 	return ψ.s
 end
+# The idea is that you can use a NormL0 object from ProximalOperators to 
+# define your problem, and inside TR and QR, you can set ψ = shifted(h, xk) 
+# to define the shifted L0-norm object. Then you call prox!() on ψ as usual.
