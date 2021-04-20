0cc7d572af14d4729268733cb9efa5a779c49648
rjbaraldi <rbaraldi@uw.edu>
2021-04-19 22:20:33 -0400

    added chi property to psi for TR algs

--- src/shiftedNormL1B2.jl
+++ src/shiftedNormL1B2.jl
@@ -5,16 +5,17 @@ mutable struct ShiftedNormL1B2{R <: Real, V1 <: AbstractVector{R}, V2 <: Abstrac
   x::V1
   s::V2
   Δ::R
-  function ShiftedNormL1B2(h::NormL1{R}, x::AbstractVector{R}, Δ::R) where {R <: Real}
+  χ::NormL2{R}
+  function ShiftedNormL1B2(h::NormL1{R}, x::AbstractVector{R}, Δ::R, χ::NormL2{R}) where {R <: Real}
     s = similar(x)
-    new{R, typeof(x), typeof(s)}(h, x, s, Δ)
+    new{R, typeof(x), typeof(s)}(h, x, s, Δ, χ)
   end
 end
 
-shifted(h::NormL1{R}, x::AbstractVector{R}, Δ::R) where {R <: Real} = ShiftedNormL1B2(h, x, Δ)
+shifted(h::NormL1{R}, x::AbstractVector{R}, Δ::R, χ::NormL2{R}) where {R <: Real} = ShiftedNormL1B2(h, x, Δ, χ)
 
-fun_name(ψ::ShiftedNormL1B2) = "shifted L1 norm with L∞-norm trust region indicator"
-fun_expr(ψ::ShiftedNormL1B2) = "s ↦ h(x + s) + χ({‖s‖ ≤ Δ})"
+fun_name(ψ::ShiftedNormL1B2) = "shifted L1 norm with L₂-norm trust region indicator"
+fun_expr(ψ::ShiftedNormL1B2) = "s ↦ h(x + s) + χ({‖s‖₂ ≤ Δ})"
 fun_params(ψ::ShiftedNormL1B2) = "x = $(ψ.x), Δ = $(ψ.Δ)"
 
 function prox(ψ::ShiftedNormL1B2{R, V1, V2}, q::AbstractVector{R}, σ::R) where {R <: Real, V1 <: AbstractVector{R}, V2 <: AbstractVector{R}}
@@ -31,5 +32,5 @@ function prox(ψ::ShiftedNormL1B2{R, V1, V2}, q::AbstractVector{R}, σ::R) where
   if norm(ψ.s) > ψ.Δ
     ψ.s .=*(ψ.Δ /norm(ψ.s))
   end
-return ψ.s
+  return ψ.s
 end
--- src/shiftedNormL1Binf.jl
+++ src/shiftedNormL1Binf.jl
@@ -5,16 +5,17 @@ mutable struct ShiftedNormL1BInf{R <: Real, V1 <: AbstractVector{R}, V2 <: Abstr
   x::V1
   s::V2
   Δ::R
-  function ShiftedNormL1BInf(h::NormL1{R}, x::AbstractVector{R}, Δ::R) where {R <: Real}
+  χ::NormLinf{R}
+  function ShiftedNormL1BInf(h::NormL1{R}, x::AbstractVector{R}, Δ::R, χ::NormLinf{R}) where {R <: Real}
     s = similar(x)
-    new{R, typeof(x), typeof(s)}(h, x, s, Δ)
+    new{R, typeof(x), typeof(s)}(h, x, s, Δ, χ)
   end
 end
 
-shifted(h::NormL1{R}, x::AbstractVector{R}, Δ::R) where {R <: Real} = ShiftedNormL1BInf(h, x, Δ)
+shifted(h::NormL1{R}, x::AbstractVector{R}, Δ::R, χ::NormLinf{R}) where {R <: Real} = ShiftedNormL1BInf(h, x, Δ, χ)
 
 fun_name(ψ::ShiftedNormL1BInf) = "shifted L1 norm with L∞-norm trust region indicator"
-fun_expr(ψ::ShiftedNormL1BInf) = "s ↦ h(x + s) + χ({‖s‖ ≤ Δ})"
+fun_expr(ψ::ShiftedNormL1BInf) = "s ↦ h(x + s) + χ({‖s‖∞ ≤ Δ})"
 fun_params(ψ::ShiftedNormL1BInf) = "x = $(ψ.x), Δ = $(ψ.Δ)"
 
 function prox(ψ::ShiftedNormL1BInf{R, V1, V2}, q::AbstractVector{R}, σ::R) where {R <: Real, V1 <: AbstractVector{R}, V2 <: AbstractVector{R}}
