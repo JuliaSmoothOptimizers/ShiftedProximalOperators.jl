7c9821c21f5d00a520270796fa79422cbc3177d4
rjbaraldi <rbaraldi@uw.edu>
2021-04-27 16:28:47 -0500

    fixed up tests for new proxes, still need to test for accuracy

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,12 @@ using ShiftedProximalOperators
 using Test
 
 # loop over operators without a trust region
-for shifted_op ∈ (:ShiftedNormL0,:ShiftedNormL1, :ShiftedIndBallL0)
+for (op, shifted_op) ∈ zip((:NormL0, :NormL1), (:ShiftedNormL0,:ShiftedNormL1))
   @testset "$shifted_op" begin
     ShiftedOp = eval(shifted_op)
+    Op = eval(op)
     # test basic types and properties
-    h = NormL0(1.2)
+    h = Op(1.2)
     x = ones(3)
     ψ = shifted(h, x)
     @test typeof(ψ) == ShiftedOp{Float64, Vector{Float64}, Vector{Float64}, Vector{Float64}}
@@ -39,7 +40,7 @@ for shifted_op ∈ (:ShiftedNormL0,:ShiftedNormL1, :ShiftedIndBallL0)
     @test φ(y) == h(x + s + y)
 
     # test different types
-    h = NormL0(Float32(1.2))
+    h = Op(Float32(1.2))
     y = rand(Float32, 10)
     x = view(y, 1:2:10)
     ψ = shifted(h, x)
@@ -50,15 +51,56 @@ for shifted_op ∈ (:ShiftedNormL0,:ShiftedNormL1, :ShiftedIndBallL0)
   end
 end
 
-# loop over operators with a trust region
-for shifted_op ∈ (:ShiftedNormL0BInf,:ShiftedNormL1BInf, :ShiftedNormL1B2, :ShiftedIndBallL0BInf)
+# loop over integer operators without a trust region
+for (op, shifted_op) ∈ zip((:IndBallL0,), (:ShiftedIndBallL0,)) ### -> add more if we come up with some? 
   @testset "$shifted_op" begin
     ShiftedOp = eval(shifted_op)
+    Op = eval(op)
     # test basic types and properties
-    h = NormL0(1.2)
+    h = Op(1)
+    x = ones(3)
+    ψ = shifted(h, x)
+    @test typeof(ψ) == ShiftedOp{Int64, Float64, Vector{Float64}, Vector{Float64}, Vector{Float64}}
+    @test all(ψ.x .== x)
+    @test typeof(ψ.r) == Int64
+    @test ψ.r == h.r
+
+    # test values
+    @test ψ(zeros(3)) == h(x)
+    y = rand(3)
+    @test ψ(y) == h(x + y)
+
+    # test prox
+    # TODO
+
+    # test shift update
+    shift!(ψ, y)
+    @test all(ψ.x .== y)
+
+    # test different types
+    h = Op(Int32(1))
+    y = rand(Float32, 10)
+    x = view(y, 1:2:10)
+    ψ = shifted(h, x)
+    @test typeof(ψ) == ShiftedOp{Int32, Float32, Vector{Float32}, SubArray{Float32, 1, Vector{Float32}, Tuple{StepRange{Int64, Int64}}, true}, Vector{Float32}}
+    @test typeof(ψ.r) == Int32
+    @test ψ.r == h.r
+    @test ψ(zeros(Int32, 5)) == h(x)
+
+  end
+end
+
+# loop over operators with a trust region
+for (op, tr, shifted_op) ∈ zip((:NormL0, :NormL1, :NormL1), (:NormLinf, :NormLinf, :NormL2), (:ShiftedNormL0BInf,:ShiftedNormL1BInf, :ShiftedNormL1B2))
+  @testset "$shifted_op" begin
+    Op = eval(op)
+    ShiftedOp = eval(shifted_op)
+    χ = eval(tr)(1.0)
+    # test basic types and properties
+    h = Op(1.2)
     x = ones(3)
     Δ = 0.5
-    ψ = shifted(h, x, Δ)
+    ψ = shifted(h, x, Δ, χ)
     @test typeof(ψ) == ShiftedOp{Float64, Vector{Float64}, Vector{Float64}, Vector{Float64}}
     @test all(ψ.x0 .== 0)
     @test all(ψ.x .== x)
@@ -84,13 +126,62 @@ for shifted_op ∈ (:ShiftedNormL0BInf,:ShiftedNormL1BInf, :ShiftedNormL1B2, :Sh
     @test ψ.Δ == 1.1
 
     # test different types
-    h = NormL0(Float32(1.2))
+    h = Op(Float32(1.2))
+    χ = eval(tr)(Float32(1.0))
     y = rand(Float32, 10)
     x = view(y, 1:2:10)
-    ψ = shifted(h, x, Float32(0.5))
+    ψ = shifted(h, x, Float32(0.5), χ)
     @test typeof(ψ) == ShiftedOp{Float32, Vector{Float32}, SubArray{Float32, 1, Vector{Float32}, Tuple{StepRange{Int64, Int64}}, true}, Vector{Float32}}
     @test typeof(ψ.λ) == Float32
     @test ψ.λ == h.lambda
+    @test ψ(zeros(Float32, 5)) == h(x)
+  end
+end
+
+
+
+# loop over operators with a trust region
+for (op, tr, shifted_op) ∈ zip((:IndBallL0, ), (:NormLinf,), (:ShiftedIndBallL0BInf, ))
+  @testset "$shifted_op" begin
+    ShiftedOp = eval(shifted_op)
+    χ = eval(tr)(1.0)
+    Op = eval(op)
+    # test basic types and properties
+    h = Op(1)
+    x = ones(3)
+    Δ = 0.5
+    ψ = shifted(h, x, Δ, χ)
+    @test typeof(ψ) == ShiftedOp{Int64, Float64, Vector{Float64}, Vector{Float64}, Vector{Float64}}
+    @test all(ψ.x .== x)
+    @test typeof(ψ.r) == Int64
+    @test ψ.r == h.r
+    @test ψ.Δ == Δ
+
+    # test values
+    @test ψ(zeros(3)) == h(x)
+    y = rand(3)
+    @test ψ(y) == h(x + y)
+
+    # test prox
+    # TODO
+
+    # test shift update
+    shift!(ψ, y)
+    @test all(ψ.x .== y)
+
+    # test radius update
+    set_radius!(ψ, 1.1)
+    @test ψ.Δ == 1.1
+
+    # test different types
+    h = Op(Int32(1))
+    χ = eval(tr)(Float32(1.0))
+    y = rand(Float32, 10)
+    x = view(y, 1:2:10)
+    ψ = shifted(h, x, Float32(0.5), χ)
+    @test typeof(ψ) == ShiftedOp{Int32, Float32, Vector{Float32}, SubArray{Float32, 1, Vector{Float32}, Tuple{StepRange{Int64, Int64}}, true}, Vector{Float32}}
+    @test typeof(ψ.r) == Int32
+    @test ψ.r == h.r
     @test ψ(zeros(Float32, 5)) == h(x)
   end
 end
