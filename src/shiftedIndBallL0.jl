e53efc2669eb342871c422eb03971018f4ba63d4
Dominique Orban <dominique.orban@gmail.com>
2021-06-05 00:50:27 -0400

    fixed shiftedIndBallL0

--- src/shiftedIndBallL0.jl
+++ src/shiftedIndBallL0.jl
@@ -8,36 +8,38 @@ mutable struct ShiftedIndBallL0{
   V2 <: AbstractVector{R},
 } <: ShiftedProximableFunction
   h::IndBallL0{I}
-  x0::V0
-  x::V1
-  s::V2
+  xk::V0
+  sj::V1
+  sol::V2
   p::Vector{Int}
+  shifted_twice::Bool
 
   function ShiftedIndBallL0(
     h::IndBallL0{I},
-    x0::AbstractVector{R},
-    x::AbstractVector{R},
+    xk::AbstractVector{R},
+    sj::AbstractVector{R},
+    shifted_twice::Bool
   ) where {I <: Integer, R <: Real}
-    s = similar(x)
-    new{I, R, typeof(x0), typeof(x), typeof(s)}(h, x0, x, s, Vector{Int}(undef, length(x)))
+    sol = similar(sj)
+    new{I, R, typeof(xk), typeof(sj), typeof(sol)}(h, xk, sj, sol, Vector{Int}(undef, length(sj)), shifted_twice)
   end
 end
 
-shifted(h::IndBallL0{I}, x::AbstractVector{R}) where {I <: Integer, R <: Real} =
-  ShiftedIndBallL0(h, zero(x), x)
+shifted(h::IndBallL0{I}, xk::AbstractVector{R}) where {I <: Integer, R <: Real} =
+  ShiftedIndBallL0(h, xk, zero(xk), false)
 shifted(
   ψ::ShiftedIndBallL0{I, R, V0, V1, V2},
-  x::AbstractVector{R},
+  sj::AbstractVector{R},
 ) where {
   I <: Integer,
   R <: Real,
   V0 <: AbstractVector{R},
   V1 <: AbstractVector{R},
   V2 <: AbstractVector{R},
-} = ShiftedIndBallL0(ψ.h, ψ.x, x)
+} = ShiftedIndBallL0(ψ.h, ψ.xk, sj, true)
 
 fun_name(ψ::ShiftedIndBallL0) = "shifted L0 norm ball indicator"
-fun_expr(ψ::ShiftedIndBallL0) = "s ↦ χ({‖x + s‖₀ ≤ r})"
+fun_expr(ψ::ShiftedIndBallL0) = "t ↦ χ({‖xk + sj + t‖₀ ≤ r})"
 
 function prox(
   ψ::ShiftedIndBallL0{I, R, V0, V1, V2},
@@ -50,10 +52,10 @@ function prox(
   V1 <: AbstractVector{R},
   V2 <: AbstractVector{R},
 }
-  ψ.s .= ψ.x .+ ψ.x0 .+ q
+  ψ.sol .= ψ.xk .+ ψ.sj .+ q
   # find largest entries
-  sortperm!(ψ.p, ψ.s, rev = true, by = abs) # stock with ψ.p as placeholder
-  ψ.s[ψ.p[(ψ.h.r + 1):end]] .= 0 # set smallest to zero
-  ψ.s .-= ψ.x .+ ψ.x0
-  return ψ.s
+  sortperm!(ψ.p, ψ.sol, rev = true, by = abs) # stock with ψ.p as placeholder
+  ψ.sol[ψ.p[(ψ.h.r + 1):end]] .= 0 # set smallest to zero
+  ψ.sol .-= ψ.xk .+ ψ.sj
+  return ψ.sol
 end
