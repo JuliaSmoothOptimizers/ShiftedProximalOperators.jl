47746b083474bd31403ed2577ad4e190937faafe
rjbaraldi <rbaraldi@uw.edu>
2021-09-27 11:33:52 -0600

    added shifted root test

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,7 +20,7 @@ end
 
 
 # loop over operators without a trust region
-for (op, shifted_op) ∈ zip((:NormL0, :NormL1), (:ShiftedNormL0, :ShiftedNormL1))
+for (op, shifted_op) ∈ zip((:NormL0, :NormL1, :RootNormLhalf), (:ShiftedNormL0, :ShiftedNormL1, :ShiftedRootNormLhalf))
   @testset "$shifted_op" begin
     ShiftedOp = eval(shifted_op)
     Op = eval(op)
