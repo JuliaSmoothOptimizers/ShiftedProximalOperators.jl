743bcc0a24557cdf0a6175eae269c1b8e21ab515
dpo <dpo@users.noreply.github.com>
2021-11-13 21:54:02 +0000

    :robot: Format .jl files

--- src/rootNormLhalf.jl
+++ src/rootNormLhalf.jl
@@ -22,17 +22,22 @@ struct RootNormLhalf{R <: Real} <: ProximableFunction
   end
 end
 
-RootNormLhalf(lambda::R=1) where {R <: Real} = RootNormLhalf{R}(lambda)
+RootNormLhalf(lambda::R = 1) where {R <: Real} = RootNormLhalf{R}(lambda)
 
 function (f::RootNormLhalf)(x::AbstractArray{T}) where {T <: Real}
   return f.lambda * T(sum(sqrt.(abs.(x))))
 end
 
-function prox!(y::AbstractArray{T}, f::RootNormLhalf, x::AbstractArray{T}, gamma::Real=1) where {T <: Real}
+function prox!(
+  y::AbstractArray{T},
+  f::RootNormLhalf,
+  x::AbstractArray{T},
+  gamma::Real = 1,
+) where {T <: Real}
   γλ = gamma * f.lambda
-  ϕ(z) = acos(γλ / 4 * (abs(z) /3 )^(-3/2))
+  ϕ(z) = acos(γλ / 4 * (abs(z) / 3)^(-3 / 2))
   ysum = zero(T)
-  threshold = 54^(1/3) * ((2γλ)^(2/3)) / 4
+  threshold = 54^(1 / 3) * ((2γλ)^(2 / 3)) / 4
   for i in eachindex(x)
     if abs(x[i]) <= threshold
       y[i] = 0
@@ -50,9 +55,13 @@ fun_dom(f::RootNormLhalf) = "AbstractArray{Real}, AbstractArray{Complex}"
 fun_expr(f::RootNormLhalf{T}) where {T <: Real} = "x ↦ ½ λ ‖x‖_(½)^(½)"
 fun_params(f::RootNormLhalf{T}) where {T <: Real} = "λ = $(f.lambda)"
 
-function prox_naive(f::RootNormLhalf, x::AbstractArray{T}, gamma::Real=1) where {R, T <: Real}
+function prox_naive(f::RootNormLhalf, x::AbstractArray{T}, gamma::Real = 1) where {R, T <: Real}
   γλ = gamma * f.lambda
-  over = abs.(x) .> 3 * (2γλ)^(2/3) / 4
-  y = (2 / 3 * sign.(x) .* abs.(x) .* (1 .+ cos.((2/3) * (π .- acos( γλ/4 * (abs.(x) ./ 3).^(-3/2))) ))) .* over
+  over = abs.(x) .> 3 * (2γλ)^(2 / 3) / 4
+  y =
+    (
+      2 / 3 * sign.(x) .* abs.(x) .*
+      (1 .+ cos.((2 / 3) * (π .- acos(γλ / 4 * (abs.(x) ./ 3) .^ (-3 / 2)))))
+    ) .* over
   return y, f.lambda * R(sum(sqrt.(abs.(y))))
 end
