621ffc844d147364d06f99cbc1fe86efa519677b
dpo <dpo@users.noreply.github.com>
2021-11-13 21:54:02 +0000

    :robot: Format .jl files

--- src/shiftedRootNormLhalfBinf.jl
+++ src/shiftedRootNormLhalfBinf.jl
@@ -29,13 +29,17 @@ end
 
 (ψ::ShiftedRootNormLhalfBinf)(y) = ψ.h(ψ.xk + ψ.sj + y) + IndBallLinf(ψ.Δ)(ψ.sj + y)
 
-shifted(h::RootNormLhalf{R}, xk::AbstractVector{R}, Δ::R, χ::Conjugate{IndBallL1{R}}) where {R <: Real} =
-ShiftedRootNormLhalfBinf(h, xk, zero(xk), Δ, χ, false)
+shifted(
+  h::RootNormLhalf{R},
+  xk::AbstractVector{R},
+  Δ::R,
+  χ::Conjugate{IndBallL1{R}},
+) where {R <: Real} = ShiftedRootNormLhalfBinf(h, xk, zero(xk), Δ, χ, false)
 shifted(
   ψ::ShiftedRootNormLhalfBinf{R, V0, V1, V2},
   sj::AbstractVector{R},
 ) where {R <: Real, V0 <: AbstractVector{R}, V1 <: AbstractVector{R}, V2 <: AbstractVector{R}} =
-ShiftedRootNormLhalfBinf(ψ.h, ψ.xk, sj, ψ.Δ, ψ.χ, true)
+  ShiftedRootNormLhalfBinf(ψ.h, ψ.xk, sj, ψ.Δ, ψ.χ, true)
 
 fun_name(ψ::ShiftedRootNormLhalfBinf) = "shifted ∑ᵢ√|⋅| norm with L∞-norm trust region indicator"
 fun_expr(ψ::ShiftedRootNormLhalfBinf) = "t ↦ ‖xk + sj + t‖ₚᵖ + χ({‖sj + t‖∞ ≤ Δ}), p = 1/2"
@@ -49,11 +53,11 @@ function prox!(
   σ::R,
 ) where {R <: Real, V0 <: AbstractVector{R}, V1 <: AbstractVector{R}, V2 <: AbstractVector{R}}
   γλ = 2 * σ * ψ.λ
-  ϕ(z) = acos(γλ / 8 * (abs(z) /3 )^(-3/2) + 0im)
+  ϕ(z) = acos(γλ / 8 * (abs(z) / 3)^(-3 / 2) + 0im)
   # p = 54^(1/3) * (γλ^(2/3)) / 4 # not necessary to compute
 
   qbar = q + (ψ.xk .+ ψ.sj)
-  RNorm(tt, l) = .5/σ*(tt - q[l])^2 + ψ.λ*sqrt(abs(tt + ψ.sj[l] + ψ.xk[l]))
+  RNorm(tt, l) = 0.5 / σ * (tt - q[l])^2 + ψ.λ * sqrt(abs(tt + ψ.sj[l] + ψ.xk[l]))
   t = zeros(4) #probably not smart to use arrays, but can change
   ft = similar(t)
   for i ∈ eachindex(q)
@@ -61,7 +65,9 @@ function prox!(
     t[1] = -ψ.sj[i] - ψ.Δ
     t[2] = -ψ.sj[i] + ψ.Δ
     t[3] = 0 - ψ.xk[i] - ψ.sj[i]
-    t[4] = real(2 * sign(qbar[i]) / 3 * aqi * (1 + cos(2 * π / 3 - 2 * ϕ(qbar[i]) / 3))) - ψ.xk[i] - ψ.sj[i]
+    t[4] =
+      real(2 * sign(qbar[i]) / 3 * aqi * (1 + cos(2 * π / 3 - 2 * ϕ(qbar[i]) / 3))) - ψ.xk[i] -
+      ψ.sj[i]
 
     for j = 1:4
       if abs(t[j] + ψ.sj[i]) ≤ ψ.Δ + eps() #necessary eps here?
