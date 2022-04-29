b28961a3bc79ed8e43669a033b7c67caf11285c8
Joshua-Wolff <joshuawolff9@gmail.com>
2022-04-29 16:49:42 -0400

    function in dev.jl script to compute the indicator of a set [l,u]

---
+++ src/dev.jl
@@ -0,0 +1,40 @@
+
+### Des commandes pour tester la lib
+
+#=
+h = NormL1(1.0)
+n = 4
+Δ = 2 * rand()
+q = 2 * (rand(n) .- 0.5)
+χ = NormLinf(1.0)
+ν = rand()
+xk = rand(n) .- 0.5
+ψ = shifted(h, xk, Δ, χ) # idee : shifted(h, xk, l, u, χ)
+ShiftedProximalOperators.prox(ψ, q, ν)
+=#
+
+
+### Pb : on fait appel a IndBallLinf qui est une fonction de la lib ProximalOperators
+### or cela correspond uniquement à une indicatrice pour une region de la forme [-a,a]
+### alors aue l'on voudrait [l,u] quelconque. la fonction ci dessous implemente ca (IndSet).
+
+using LinearAlgebra
+using ProximalOperators
+
+struct IndSet
+    l::Real
+    u::Real
+    IndSet(l,u) = l > u ? error("out of order") : new(l,u)
+end
+
+
+function (f::IndSet)(x)
+    ind = zeros(Float64,size(x))
+    ind[.!(l .<= x .<= u)] .= Inf
+    return ind
+end
+
+
+
+
+
