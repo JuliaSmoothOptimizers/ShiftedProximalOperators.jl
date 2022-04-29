c05181afc693c0212e4cedf7c6d539f109815fcc
Joshua-Wolff <joshuawolff9@gmail.com>
2022-04-29 16:49:42 -0400

    use general boxes instead of trust regions
    boxes will allow the solution of bound-constrained problems.

---
+++ src/shiftedNormL0Box.jl
@@ -0,0 +1,141 @@
+export ShiftedNormL0Box
+
+mutable struct ShiftedNormL0Box{
+  R <: Real,
+  T <: Integer,
+  V0 <: AbstractVector{R},
+  V1 <: AbstractVector{R},
+  V2 <: AbstractVector{R},
+  V3,
+  V4,
+} <: ShiftedProximableFunction
+  h::NormL0{R}
+  xk::V0
+  sj::V1
+  sol::V2
+  l::V3
+  u::V4
+  Δ::R
+  shifted_twice::Bool
+  selected::UnitRange{T}
+
+  function ShiftedNormL0Box(
+    h::NormL0{R},
+    xk::AbstractVector{R},
+    sj::AbstractVector{R},
+    l,
+    u,
+    Δ::R,
+    shifted_twice::Bool,
+    selected::UnitRange{T},
+  ) where {R <: Real, T <: Integer}
+    sol = similar(xk)
+    if any(l .> u)
+      error("Error: at least one lower bound is greater than the upper bound.")
+    end
+    new{R, T, typeof(xk), typeof(sj), typeof(sol), typeof(l), typeof(u)}(
+      h,
+      xk,
+      sj,
+      sol,
+      l,
+      u,
+      Δ,
+      shifted_twice,
+      selected,
+    )
+  end
+end
+
+shifted(h::NormL0{R}, xk::AbstractVector{R}, l, u, Δ::R) where {R <: Real} =
+  ShiftedNormL0Box(h, xk, zero(xk), l, u, Δ, false, 1:length(xk))
+shifted(
+  h::NormL0{R},
+  xk::AbstractVector{R},
+  l,
+  u,
+  Δ::R,
+  selected::UnitRange{T},
+) where {R <: Real, T <: Integer} = ShiftedNormL0Box(h, xk, zero(xk), l, u, Δ, false, selected)
+shifted(
+  ψ::ShiftedNormL0Box{R, T, V0, V1, V2, V3, V4},
+  sj::AbstractVector{R},
+) where {
+  R <: Real,
+  T <: Integer,
+  V0 <: AbstractVector{R},
+  V1 <: AbstractVector{R},
+  V2 <: AbstractVector{R},
+  V3,
+  V4,
+} = ShiftedNormL0Box(ψ.h, ψ.xk, sj, ψ.l, ψ.u, ψ.Δ, true, ψ.selected)
+
+function (ψ::ShiftedNormL0Box)(y)
+  val = ψ.h((ψ.xk + ψ.sj + y)[ψ.selected])
+  ϵ = √eps(eltype(y))
+  for i ∈ eachindex(y)
+    lower = isa(ψ.l, Real) ? ψ.l : ψ.l[i]
+    upper = isa(ψ.u, Real) ? ψ.u : ψ.u[i]
+    if !(lower - ϵ ≤ ψ.sj[i] + y[i] ≤ upper + ϵ)
+      return Inf
+    end
+  end
+  return val
+end
+
+fun_name(ψ::ShiftedNormL0Box) = "shifted L0 pseudo-norm with box indicator"
+fun_expr(ψ::ShiftedNormL0Box) = "t ↦ λ ‖xk + sj + t‖₀ + χ({sj + t .∈ [l,u]})"
+fun_params(ψ::ShiftedNormL0Box) =
+  "xk = $(ψ.xk)\n" * " "^14 * "sj = $(ψ.sj)\n" * " "^14 * "lb = $(ψ.l)\n" * " "^14 * "ub = $(ψ.u)"
+
+function prox!(
+  y::AbstractVector{R},
+  ψ::ShiftedNormL0Box{R, T, V0, V1, V2, V3, V4},
+  q::AbstractVector{R},
+  σ::R,
+) where {
+  R <: Real,
+  T <: Integer,
+  V0 <: AbstractVector{R},
+  V1 <: AbstractVector{R},
+  V2 <: AbstractVector{R},
+  V3,
+  V4,
+}
+  c = 2 * ψ.λ * σ
+  selected = ψ.selected
+
+  for i ∈ eachindex(q)
+    li = isa(ψ.l, Real) ? ψ.l : ψ.l[i]
+    ui = isa(ψ.u, Real) ? ψ.u : ψ.u[i]
+
+    qi = q[i]
+    si = ψ.sj[i]
+    sq = si + qi
+
+    if i ∈ selected
+      xi = ψ.xk[i]
+      xs = xi + si
+      xsq = xs + qi
+      val_left = (li - sq)^2 + (xi == -li ? 0 : c)
+      val_right = (ui - sq)^2 + (xi == -ui ? 0 : c)
+      # subtract x + s from solution explicitly here instead of doing it
+      # numerically at the end
+      y[i] = val_left < val_right ? (li - si) : (ui - si)
+      val_min = min(val_left, val_right)
+      if li ≤ -xi ≤ ui  # <=> li + xi ≤ 0 ≤ ui + xi
+        val_0 = xsq^2
+        val_0 < val_min && (y[i] = -xs)
+        val_min = min(val_0, val_min)
+      end
+      if li ≤ sq ≤ ui  # <=> li + xi ≤ xsq ≤ ui + xi
+        val_xsq = xsq == 0 ? zero(R) : c
+        val_xsq < val_min && (y[i] = qi)
+      end
+
+    else # min ½ σ⁻¹ (y - qi)² subject to li - si ≤ y ≤ ui - si
+      y[i] = min(max(q[i], li - si), ui - si)
+    end
+  end
+  return y
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -313,7 +313,7 @@ end
 for (op, tr, shifted_op) ∈ zip(
   (:NormL0, :NormL1, :NormL1, :RootNormLhalf),
   (:NormLinf, :NormLinf, :NormL2, :NormLinf),
-  (:ShiftedNormL0BInf, :ShiftedNormL1BInf, :ShiftedNormL1B2, :ShiftedRootNormLhalfBinf),
+  (:ShiftedNormL0Box, :ShiftedNormL1Box, :ShiftedNormL1B2, :ShiftedRootNormLhalfBinf),
 )
   @testset "$shifted_op" begin
     ShiftedOp = eval(shifted_op)
@@ -324,18 +324,21 @@ for (op, tr, shifted_op) ∈ zip(
     h = Op(1.0)
     x = ones(n)
     Δ = 0.01
-    ψ = shifted(h, x, Δ, χ)
-    @test typeof(ψ) == ShiftedOp{Float64, Vector{Float64}, Vector{Float64}, Vector{Float64}}
+    if "$shifted_op" ∈ ("ShiftedNormL0Box", "ShiftedNormL1Box")
+      ψ = shifted(h, x, -Δ, Δ, Δ)
+    else
+      ψ = shifted(h, x, Δ, χ)
+      @test typeof(ψ) == ShiftedOp{Float64, Vector{Float64}, Vector{Float64}, Vector{Float64}}
+    end
     @test all(ψ.sj .== 0)
     @test all(ψ.xk .== x)
     @test typeof(ψ.λ) == Float64
     @test ψ.λ == h.lambda
     @test ψ.Δ == Δ
-
     # test values
     @test ψ(zeros(n)) == h(x)
     y = rand(n)
-    y .*= ψ.Δ / ψ.χ(y) / 2
+    y .*= ψ.Δ / χ(y) / 2
     @test ψ(y) == h(x + y)  # y inside the trust region
     @test ψ(3 * y) == Inf   # y outside the trust region
 
@@ -349,7 +352,7 @@ for (op, tr, shifted_op) ∈ zip(
         1770.8953574224836,
         -2554.7769423950244,
       ]
-    if "$shifted_op" == "ShiftedNormL0BInf"
+    if "$shifted_op" == "ShiftedNormL0Box"
       s_correct = [
         -0.010000000000000,
         0.005867144197216,
@@ -365,7 +368,7 @@ for (op, tr, shifted_op) ∈ zip(
         -0.004285677352167,
         0.006176811716709,
       ]
-    elseif "$shifted_op" == "ShiftedNormL1BInf"
+    elseif "$shifted_op" == "ShiftedNormL1Box"
       s_correct = [
         -0.010000000000000,
         0.005856155186227,
@@ -384,7 +387,7 @@ for (op, tr, shifted_op) ∈ zip(
     end
     s = ShiftedProximalOperators.prox(ψ, q, ν)
     @test all(s .≈ s_correct)
-    @test ψ.χ(s) ≤ ψ.Δ
+    @test χ(s) ≤ ψ.Δ
 
     # test shift update
     shift!(ψ, y)
@@ -393,17 +396,22 @@ for (op, tr, shifted_op) ∈ zip(
 
     # test radius update
     set_radius!(ψ, 1.1)
+    if "$shifted_op" ∈ ("ShiftedNormL0Box", "ShiftedNormL1Box")
+      set_bounds!(ψ, -1.1, 1.1)
+      @test ψ.l == -1.1
+      @test ψ.u == 1.1
+    end
     @test ψ.Δ == 1.1
 
     # shift a shifted operator
     s = ones(n)
-    s /= 2 * ψ.χ(s)
+    s /= 2 * χ(s)
     φ = shifted(ψ, s)
     @test all(φ.sj .== s)
     @test all(φ.xk .== x)
     @test φ(zeros(n)) == h(y + s)
     t = rand(n)
-    t .*= ψ.Δ / ψ.χ(t) / 2
+    t .*= ψ.Δ / χ(t) / 2
     @test φ(t) == h(y + s + t)  # y inside the trust region
     @test φ(3 * t) == Inf       # y outside the trust region
 
@@ -412,13 +420,19 @@ for (op, tr, shifted_op) ∈ zip(
     χ = eval(tr)(Float32(1.0))
     y = rand(Float32, 10)
     x = view(y, 1:2:10)
-    ψ = shifted(h, x, Float32(0.5), χ)
-    @test typeof(ψ) == ShiftedOp{
-      Float32,
-      SubArray{Float32, 1, Vector{Float32}, Tuple{StepRange{Int64, Int64}}, true},
-      Vector{Float32},
-      Vector{Float32},
-    }
+    if "$shifted_op" ∈ ("ShiftedNormL0Box", "ShiftedNormL1Box")
+      ψ = shifted(h, x, Float32(-0.5), Float32(0.5), Float32(0.5))
+    else
+      ψ = shifted(h, x, Float32(0.5), χ)
+    end
+    if "$shifted_op" ∉ ("ShiftedNormL0Box", "ShiftedNormL1Box")
+      @test typeof(ψ) == ShiftedOp{
+        Float32,
+        SubArray{Float32, 1, Vector{Float32}, Tuple{StepRange{Int64, Int64}}, true},
+        Vector{Float32},
+        Vector{Float32},
+      }
+    end
     @test typeof(ψ.λ) == Float32
     @test ψ.λ == h.lambda
     @test ψ(zeros(Float32, 5)) == h(x)
@@ -696,7 +710,7 @@ for (op, tr) ∈ zip((:NormL1,), (:NormLinf,))
 
     # shift once
     xk = rand(n) .- 0.5
-    ψ = shifted(h, xk, Δ, χ)
+    ψ = shifted(h, xk, -Δ, Δ, Δ)
 
     # check prox
     p1 = ProximalOperators.prox(h, xk + q, ν)[1]
@@ -1062,3 +1076,5 @@ for (op, shifted_op) ∈ zip((:Nuclearnorm,), (:ShiftedNuclearnorm,))
     @test all(Q.U * Diagonal(t) * Q.Vt - reshape(x + s, m, n) .≈ reshape(prox!(y, f, q, γ), m, n))
   end
 end
+
+include("testsbox.jl")
