e557163d8517c3c07cbc05761f6134569fb93452
rjbaraldi <rbaraldi@uw.edu>
2022-06-14 18:59:19 -0600

    added if/then for norm=0 case

--- src/groupNormL2.jl
+++ src/groupNormL2.jl
@@ -36,39 +36,46 @@ end
 
 GroupNormL2(
   lambda::RR = [1.],
-  idx::I = [Int[]],
+  idx::I = [[]],
 ) where {R <: Real, RR <: AbstractVector{R}, I} =
 GroupNormL2{R, RR, I}(lambda, idx)
 
-function (f::GroupNormL2)(x::AbstractArray{T}) where {T <: Real}
-  sum_c = T(0)
+function (f::GroupNormL2)(x::AbstractArray{R}) where {R <: Real}
+  sum_c = R(0)
   if length(f.idx) == 1
-    return f.lambda * sqrt(sum(x.^2))
+    return f.lambda * norm(x)
   else
     for i = 1:length(f.idx)
-      sum_c += f.lambda[i]*sqrt(sum(x[f.idx[i]].^2))
+      sum_c += f.lambda[i]*norm(x[f.idx[i]])
     end
     return sum_c
   end
 end
 
 function prox!(
-  y::AbstractArray{T},
-  f::GroupNormL2{T, R, I},
-  x::AbstractArray{T},
-  γ::T = T(1),
-) where {T <: Real, R <: AbstractVector{T}, I}
+  y::AbstractArray{R},
+  f::GroupNormL2{R, RR, I},
+  x::AbstractArray{R},
+  γ::R = R(1),
+) where {R <: Real, RR <: AbstractVector{R}, I}
 
-  ysum = T(0)
-  yt = T(0)
+  ysum = R(0)
   if length(f.idx) == 1
     ysum = f.lambda*norm(x)
-    y .= max(1 .- γ * f.lambda / ysum, 0) .* x
+    if ysum == 0
+      y .= 0
+    else
+      y .= max(1 .- γ * f.lambda / ysum, 0) .* x
+    end
   else
     for i = 1:length(f.idx)
       yt = norm(x[f.idx[i]])
-      ysum += f.lambda[i]*yt
-      y[f.idx[i]] .= max(1 .- γ*f.lambda[i]/yt, 0) .* x[f.idx[i]]
+      if yt == 0
+        y[f.idx[i]] .= 0
+      else
+        y[f.idx[i]] .= max(1 .- γ*f.lambda[i]/yt, 0) .* x[f.idx[i]]
+        ysum += f.lambda[i]*yt
+      end
     end
   end
   return ysum
