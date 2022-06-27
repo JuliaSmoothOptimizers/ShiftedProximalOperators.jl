c28cdc4e9028db57f781ea9a441e22cc79f47d37
Dominique <dominique.orban@gmail.com>
2022-06-27 17:35:41 -0400

    Update groupNormL2.jl

--- src/groupNormL2.jl
+++ src/groupNormL2.jl
@@ -7,7 +7,7 @@ export GroupNormL2
 
 Returns the group ``\ell_2``-norm operator
 ```math
-f(x) =  \sum_i \\ambda_i \|x_{[i]}\|_2^{1/2}
+f(x) =  \sum_i \lambda_i \|x_{[i]}\|_2^{1/2}
 ```
 for groups ``x_{[i]}`` and nonnegative weights ``\lambda_i``.
 The group ``\ell_2``-norm operator reduces to the ``\ell_2``-norm if only one group is defined (the default).
