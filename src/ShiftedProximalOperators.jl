87223efd00ea336a17ac876150d3b403ac74d603
rjbaraldi <rbaraldi@uw.edu>
2022-06-29 15:05:40 -0600

    updated group/shifted norml2 tests, attempt 2

--- src/ShiftedProximalOperators.jl
+++ src/ShiftedProximalOperators.jl
@@ -19,8 +19,9 @@ include("groupNormL2.jl")
 include("shiftedNormL0.jl")
 include("shiftedNormL0BInf.jl")
 include("shiftedRootNormLhalf.jl")
-
 include("shiftedNormL1.jl")
+include("shiftedGroupNormL2.jl")
+
 include("shiftedNormL1B2.jl")
 include("shiftedNormL1Binf.jl")
 include("shiftedIndBallL0.jl")
