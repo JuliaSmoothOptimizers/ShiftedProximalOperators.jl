f506ad2a3879e7c6c3502b0f45f9c7500df0e93e
Geoffroy Leconte <gleconte50@gmail.com>
2023-03-24 17:08:49 -0400

    update iprox doc

--- src/ShiftedProximalOperators.jl
+++ src/ShiftedProximalOperators.jl
@@ -139,12 +139,16 @@ prox!
 
 Evaluate the indefinite proximal operator of a separable box shifted regularizer, i.e, return
 a solution y of
-    minimize{yᵢ}  ½ dᵢyᵢ² + gᵢ yᵢ + ψ(yᵢ), i ∈ {1, ..., length(y)}
+
+    minimize{y}  ½ yᵀDy + gᵀy + ψ(y)
+
 where
+
 * ψ is a `ShiftedProximableFunction` representing a model of the sum of a separable function h(x + s) and
   the indicator of a trust region;
 * g is a vector;
-* d is a vector.
+* `D = Diagonal(d)` where d is a vector.
+
 The solution is stored in the input vector `y` an `y` is returned.
 """
 iprox!
