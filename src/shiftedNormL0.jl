394725dbb60261445021e69c9e0d786fe5f33fc6
MohamedLaghdafHABIBOULLAH <mohamed-laghdaf-2.habiboullah@polymtl.ca>
2024-09-17 01:55:03 -0400

    generalization of iprox for L0 and L1 norm if D is not positive

--- src/shiftedNormL0.jl
+++ src/shiftedNormL0.jl
@@ -67,13 +67,17 @@ function iprox!(
   λ = ψ.h.lambda
   for i ∈ eachindex(y)
     di = d[i]
-    @assert di > 0
-    ci = sqrt(2 * λ * di)
-    xps = ψ.xk[i] + ψ.sj[i]
-    if abs(di * xps - g[i]) ≤ ci
-      y[i] = -xps
-    else
-      y[i] = -g[i] / di
+    if d[i] ≤ 0
+      y[i] = - 1/eps(R) 
+      continue
+    else 
+      ci = sqrt(2 * λ * di)
+      xps = ψ.xk[i] + ψ.sj[i]
+      if abs(di * xps - g[i]) ≤ ci
+        y[i] = -xps
+      else
+        y[i] = -g[i] / di
+      end
     end
   end
   return y
