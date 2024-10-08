6374874827976d6999a9805d753c8daafe45a88e
Maxence Gollier <mgollier45@gmail.com>
2024-10-08 11:18:53 -0400

    update dependencies

--- src/ShiftedProximalOperators.jl
+++ src/ShiftedProximalOperators.jl
@@ -1,7 +1,6 @@
 module ShiftedProximalOperators
 
 using LinearAlgebra
-using SparseArrays
 using QRMumps
 using SparseMatricesCOO
 
@@ -9,6 +8,7 @@ using libblastrampoline_jll
 using OpenBLAS32_jll
 using ProximalOperators
 using Roots
+using SparseArrays
 
 function __init__()
   # Ensure LBT points to a valid BLAS for psvd()
