585706392b977f9d71710c013cbf17acb0872d52
Maxence Gollier <mgollier45@gmail.com>
2025-01-29 18:19:53 -0500

    add qrm utility functions, improve readibility in shiftedCompositeL2 prox, QRMumps compat required at this point

--- src/shiftedCompositeNormL2.jl
+++ src/shiftedCompositeNormL2.jl
@@ -32,14 +32,12 @@ mutable struct ShiftedCompositeNormL2{
   c!::V0
   J!::V1
   A::V2
-  Aα::V2
-  spmat::qrm_spmat{R}
+  shifted_spmat::qrm_shifted_spmat{R}
   spfct::qrm_spfct{R}
   b::V3
   g::V3
-  res::V3
-  sol::V3
-  dsol::V3
+  q::V3
+  dq::V3
   p::V3
   dp::V3
   function ShiftedCompositeNormL2(
@@ -52,23 +50,19 @@ mutable struct ShiftedCompositeNormL2{
     p = similar(b, A.n + A.m)
     dp = similar(b, A.n + A.m)
     g = similar(b)
-    res = similar(b)
-    sol = similar(b)
-    dsol = similar(b)
-    Aα = SparseMatrixCOO(A.m,A.n + A.m, similar(A.rows,length(A.rows) + A.m), similar(A.cols, length(A.cols) + A.m), similar(A.vals,length(A.vals) + A.m))
+    q = similar(b)
+    dq = similar(b)
     if length(b) != size(A,1)
       error("ShiftedCompositeNormL2: Wrong input dimensions, there should be as many constraints as rows in the Jacobian")
     end
-    Aα.rows[1:length(A.rows)] .= A.rows
-    Aα.rows[length(A.rows)+1:end] .= eltype(A.rows)(1):eltype(A.rows)(A.m)
-    Aα.cols[1:length(A.cols)] .= A.cols
-    Aα.cols[length(A.cols)+1:end] .= eltype(A.cols)(A.n+1):eltype(A.cols)(A.n + A.m)
 
-    spmat = qrm_spmat_init(Aα; sym=false)
+    spmat = qrm_spmat_init(A; sym=false)
+    shifted_spmat = qrm_shift_spmat(spmat)
     spfct = qrm_spfct_init(spmat)
     qrm_set(spfct, "qrm_keeph", 0) # Discard de Q matrix in all subsequent QR factorizations
     qrm_set(spfct, "qrm_rd_eps", eps(R)^(0.4)) # If a diagonal elemnt of the R-factor is less than eps(R)^(0.4), we consider that A is rank defficient.
-    new{R,typeof(c!),typeof(J!),typeof(A),typeof(b)}(NormL2(λ), c!, J!, A, Aα, spmat, spfct, b, g, res, sol, dsol, p, dp)
+
+    new{R,typeof(c!),typeof(J!),typeof(A),typeof(b)}(NormL2(λ), c!, J!, A, shifted_spmat, spfct, b, g, q, dq, p, dp)
   end
 end
  
@@ -101,69 +95,45 @@ function prox!(
   α = R(0.0)
   αmin = eps(R)^(0.75)
 
-  # Initialize Aα
-  ψ.Aα.vals[1:length(ψ.A.vals)] .= ψ.A.vals
-  ψ.Aα.vals[length(ψ.A.vals)+1:end] .= eltype(ψ.A.vals)(0)
-
+  # Compute RHS
   mul!(ψ.g, ψ.A, q)
   ψ.g .+= ψ.b
   ψ.g .*= -1
 
-  spmat = ψ.spmat
+  # Retrieve qrm workspace
+  shifted_spmat = ψ.shifted_spmat
+  spmat = shifted_spmat.spmat
   spfct = ψ.spfct
+  qrm_update_shift_spmat!(shifted_spmat, α)
 
-  qrm_update!(spmat, ψ.Aα.vals)
   # Check interior convergence
   qrm_analyse!(spmat, spfct; transp='t')
-  qrm_factorize!(spmat, spfct, transp='t')
-
-  qrm_solve!(spfct, ψ.g, ψ.p, transp='t')
-  qrm_solve!(spfct, ψ.p, ψ.sol, transp='n')
-  qrm_solve!(spfct, ψ.sol, ψ.p, transp='t')
-  _iterative_refinement!(spfct,ψ)
+  _obj_dot_grad!(spmat, spfct, ψ.p, ψ.q, ψ.g, ψ.dq)
   
+  # Check full-rankness
   full_row_rank = !(qrm_get(spfct,"qrm_rd_num") > 0)
   if !full_row_rank
-    #TODO: once Golub-Riley has been implemented in QRMumps, this should be replaced with a call to qrm_golub_riley
-    α = αmin
-    
-    ψ.Aα.vals[length(ψ.A.vals)+1:end] .= eltype(ψ.A.vals)(sqrt(α))
-    qrm_update!(spmat, ψ.Aα.vals)
-    qrm_factorize!(spmat, spfct, transp='t')
-
-    qrm_solve!(spfct, ψ.g, ψ.p, transp='t')
-    qrm_solve!(spfct, ψ.p, ψ.sol, transp='n')
-    qrm_solve!(spfct, ψ.sol, ψ.p, transp='t')
-    _iterative_refinement!(spfct, ψ)
-
-    α₊ = α + (norm(ψ.sol)/(σ*ψ.h.lambda) - 1.0)*(norm(ψ.sol)/norm(ψ.p))^2
-    if norm(ψ.sol) ≤ σ*ψ.h.lambda + eps(R) && α₊ ≤ α    # We consider ψ.sol is a good approximation of the least-norm solution
-      mul!(y, ψ.A', ψ.sol)
-      y .+= q
-      return y
-    end
+    qrm_golub_riley!(ψ.shifted_spmat, spfct, ψ.p, ψ.g, ψ.dp, ψ.q, transp = 't')
+    y .= ψ.p[1:length(y)]
+    y .+= q
+    return y 
   end
   
   # Scalar Root finding
   k = 0
   elapsed_time = time() - start_time
   α₊ = α 
-  if norm(ψ.sol) > σ*ψ.h.lambda || !full_row_rank
-    while norm(ψ.sol) > σ*ψ.h.lambda + eps(R)^0.75 && k < max_iter && elapsed_time < max_time
+  if norm(ψ.q) > σ*ψ.h.lambda || !full_row_rank
+    while norm(ψ.q) > σ*ψ.h.lambda + eps(R)^0.75 && k < max_iter && elapsed_time < max_time
 
-      solNorm = norm(ψ.sol)
+      solNorm = norm(ψ.q)
       α₊ += (solNorm / (σ * ψ.h.lambda) - 1) * (solNorm / norm(ψ.p))^2
       α = α₊ > 0 ? α₊ : θ*α
       α = α ≤ αmin ? αmin : α
       
-      ψ.Aα.vals[length(ψ.A.vals)+1:end] .= eltype(ψ.A.vals)(sqrt(α))
-      qrm_update!(spmat, ψ.Aα.vals)
-      qrm_factorize!(spmat,spfct, transp='t')
+      qrm_update_shift_spmat!(shifted_spmat, α)
 
-      qrm_solve!(spfct, ψ.g, ψ.p, transp='t')
-      qrm_solve!(spfct, ψ.p, ψ.sol, transp='n')
-      qrm_solve!(spfct, ψ.sol, ψ.p, transp='t')
-      _iterative_refinement!(spfct, ψ)
+      _obj_dot_grad!(spmat, spfct, ψ.p, ψ.q, ψ.g, ψ.dq)
 
       α == αmin && break
       
@@ -173,31 +143,16 @@ function prox!(
   end
 
   #Sometimes alpha tends to 0, we don't to print the residual in this case, it is usually huge.
-  (k > max_iter && α > eps(R)) && @warn "ShiftedCompositeNormL2: Newton method did not converge during prox computation returning with residue $(abs(norm(ψ.sol) - σ*ψ.h.lambda)) instead"
-  mul!(y, ψ.A', ψ.sol)
+  (k > max_iter && α > eps(R)) && @warn "ShiftedCompositeNormL2: Newton method did not converge during prox computation returning with residue $(abs(norm(ψ.q) - σ*ψ.h.lambda)) instead"
+  mul!(y, ψ.A', ψ.q)
   y .+= q
   return y
 end
 
-#TODO: remove this and use qrm_iterative_refinement 
-function _iterative_refinement!(
-  spfct, 
-  ψ::ShiftedCompositeNormL2{R, V0, V1, V2, V3}; 
-  atol = sqrt(eps(R)), 
-  rtol = sqrt(eps(R))
-  ) where{R, V0, V1, V2, V3}
-
-  ψ.res .= ψ.g
-
-  mul!(ψ.dp, ψ.Aα', ψ.sol)
-  mul!(ψ.dsol, ψ.Aα, ψ.dp)
-
-  ψ.res .-= ψ.dsol
-  if norm(ψ.res) > atol + rtol*norm(ψ.g)
-    qrm_solve!(spfct, ψ.res, ψ.dp, transp='t')
-    qrm_solve!(spfct, ψ.dp, ψ.dsol, transp='n')
-    qrm_solve!(spfct, ψ.dsol, ψ.dp, transp='t')
-    ψ.sol .+= ψ.dsol
-    ψ.p .+= ψ.dp
-  end
+function _obj_dot_grad!(spmat :: qrm_spmat{T}, spfct :: qrm_spfct{T}, p :: AbstractVector{T}, q :: AbstractVector{T}, g :: AbstractVector{T}, dq :: AbstractVector{T}) where T
+  qrm_factorize!(spmat, spfct, transp='t')
+  qrm_solve!(spfct, g, p, transp='t')
+  qrm_solve!(spfct, p, q, transp='n')
+  qrm_refine!(spmat, spfct, q, g, dq, p)
+  qrm_solve!(spfct, q, p, transp='t')
 end
