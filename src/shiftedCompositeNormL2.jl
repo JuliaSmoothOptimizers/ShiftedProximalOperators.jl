d3c2fe383acefc704239321930e6f9b57262b46b
dpo <dpo@users.noreply.github.com>
2025-04-09 14:40:52 +0000

    :robot: Format .jl files

--- src/shiftedCompositeNormL2.jl
+++ src/shiftedCompositeNormL2.jl
@@ -54,21 +54,42 @@ mutable struct ShiftedCompositeNormL2{
     q = similar(b)
     dq = similar(b)
     if length(b) != size(A, 1)
-      error("ShiftedCompositeNormL2: Wrong input dimensions, there should be as many constraints as rows in the Jacobian")
+      error(
+        "ShiftedCompositeNormL2: Wrong input dimensions, there should be as many constraints as rows in the Jacobian",
+      )
     end
 
-    spmat = qrm_spmat_init(A; sym=false)
+    spmat = qrm_spmat_init(A; sym = false)
     shifted_spmat = qrm_shift_spmat(spmat)
     spfct = qrm_spfct_init(spmat)
-  
-    new{T, typeof(c!), typeof(J!), typeof(A), typeof(b)}(NormL2(λ), c!, J!, A, shifted_spmat, spfct, b, g, q, dq, p, dp)
+
+    new{T, typeof(c!), typeof(J!), typeof(A), typeof(b)}(
+      NormL2(λ),
+      c!,
+      J!,
+      A,
+      shifted_spmat,
+      spfct,
+      b,
+      g,
+      q,
+      dq,
+      p,
+      dp,
+    )
   end
 end
- 
+
 shifted(
   ψ::CompositeNormL2{T, F0, F1, M, V},
-  xk::AbstractVector{T}
-) where {T <: Real, F0 <: Function, F1 <: Function, M <: AbstractMatrix{T}, V <: AbstractVector{T}} = begin
+  xk::AbstractVector{T},
+) where {
+  T <: Real,
+  F0 <: Function,
+  F1 <: Function,
+  M <: AbstractMatrix{T},
+  V <: AbstractVector{T},
+} = begin
   b = similar(ψ.b)
   ψ.c!(b, xk)
   A = similar(ψ.A)
@@ -87,9 +108,8 @@ function prox!(
   ν::T;
   max_iter = 10,
   atol = eps(T)^0.3,
-  max_time = 180.0
-) where {T <: Real, F0 <: Function, F1 <:Function, M <: AbstractMatrix{T}, V <: AbstractVector{T}}
-
+  max_time = 180.0,
+) where {T <: Real, F0 <: Function, F1 <: Function, M <: AbstractMatrix{T}, V <: AbstractVector{T}}
   start_time = time()
   θ = T(0.8)
   α = zero(T)
@@ -104,21 +124,32 @@ function prox!(
   spmat = shifted_spmat.spmat
   spfct = ψ.spfct
   qrm_update_shift_spmat!(shifted_spmat, α)
-  spmat.val[1:spmat.mat.nz - spmat.mat.m] .= ψ.A.vals
+  spmat.val[1:(spmat.mat.nz - spmat.mat.m)] .= ψ.A.vals
   qrm_spfct_init!(spfct, spmat)
   qrm_set(spfct, "qrm_keeph", 0) # Discard de Q matrix in all subsequent QR factorizations
   qrm_set(spfct, "qrm_rd_eps", eps(T)^(0.4)) # If a diagonal element of the R-factor is less than eps(R)^(0.4), we consider that A is rank defficient.
 
   # Check interior convergence
-  qrm_analyse!(spmat, spfct; transp='t')
+  qrm_analyse!(spmat, spfct; transp = 't')
   _obj_dot_grad!(spmat, spfct, ψ.p, ψ.q, ψ.g, ψ.dq)
-  
+
   # Check full-rankness
-  full_row_rank = (qrm_get(spfct,"qrm_rd_num") == 0)
+  full_row_rank = (qrm_get(spfct, "qrm_rd_num") == 0)
   if !full_row_rank
     # QRMumps cannot factorize rank-deficient matrices; use the Golub-Riley iteration instead
     α = αmin
-    qrm_golub_riley!(ψ.shifted_spmat, spfct, ψ.p, ψ.g, ψ.dp, ψ.q, ψ.dq, transp = 't', α = α, tol = eps(T)^(0.75)) # Now, ψ.p = Aᵀψ.q and ψ.q = (AAᵀ)†b
+    qrm_golub_riley!(
+      ψ.shifted_spmat,
+      spfct,
+      ψ.p,
+      ψ.g,
+      ψ.dp,
+      ψ.q,
+      ψ.dq,
+      transp = 't',
+      α = α,
+      tol = eps(T)^(0.75),
+    ) # Now, ψ.p = Aᵀψ.q and ψ.q = (AAᵀ)†b
 
     # Compute residual
     qrm_spmat_mv!(spmat, T(1), ψ.q, T(0), ψ.dp, transp = 't')
@@ -128,51 +159,58 @@ function prox!(
     if abs(norm(ψ.q) - ν*ψ.h.lambda) < atol && norm(ψ.dq) ≤ eps(T)^(0.5) # Check interior optimality and range of AAᵀ
       y .= ψ.p[1:length(y)]
       y .+= q
-      return y 
+      return y
     end
 
     # The solution is not α = 0, prepare root finding
     qrm_update_shift_spmat!(shifted_spmat, α)
     _obj_dot_grad!(spmat, spfct, ψ.p, ψ.q, ψ.g, ψ.dq)
   end
-  
+
   # Scalar Root finding
   k = 0
   elapsed_time = time() - start_time
-  α₊ = α 
+  α₊ = α
 
   norm_q = norm(ψ.q)
   νλ = ν * ψ.h.lambda
   if abs(norm_q - νλ) > atol
     while abs(norm_q - νλ) > atol && k < max_iter && elapsed_time < max_time
-
       α₊ += (norm_q / νλ - 1) * (norm_q / norm(ψ.p))^2
       α = α₊ > 0 ? α₊ : θ*α
       α = α ≤ αmin ? αmin : α
-      
+
       qrm_update_shift_spmat!(shifted_spmat, α)
 
       _obj_dot_grad!(spmat, spfct, ψ.p, ψ.q, ψ.g, ψ.dq)
       norm_q = norm(ψ.q)
 
       α == αmin && break
-      
+
       k += 1
       elapsed_time = time() - start_time
     end
   end
 
-  k > max_iter && @warn "ShiftedCompositeNormL2: Newton method did not converge during prox computation returning with residual $(abs(norm(ψ.q) - ν*ψ.h.lambda)) instead"
+  k > max_iter &&
+    @warn "ShiftedCompositeNormL2: Newton method did not converge during prox computation returning with residual $(abs(norm(ψ.q) - ν*ψ.h.lambda)) instead"
   mul!(y, ψ.A', ψ.q)
   y .+= q
   return y
 end
 
 # Utility function that computes in place both q = s(α) and p such that ‖p‖² = s(α)ᵀ∇s(α) for the secular equation.
-function _obj_dot_grad!(spmat :: qrm_spmat{T}, spfct :: qrm_spfct{T}, p :: AbstractVector{T}, q :: AbstractVector{T}, g :: AbstractVector{T}, dq :: AbstractVector{T}) where T
-  qrm_factorize!(spmat, spfct, transp='t')
-  qrm_solve!(spfct, g, p, transp='t')
-  qrm_solve!(spfct, p, q, transp='n')
+function _obj_dot_grad!(
+  spmat::qrm_spmat{T},
+  spfct::qrm_spfct{T},
+  p::AbstractVector{T},
+  q::AbstractVector{T},
+  g::AbstractVector{T},
+  dq::AbstractVector{T},
+) where {T}
+  qrm_factorize!(spmat, spfct, transp = 't')
+  qrm_solve!(spfct, g, p, transp = 't')
+  qrm_solve!(spfct, p, q, transp = 'n')
   qrm_refine!(spmat, spfct, q, g, dq, p)
-  qrm_solve!(spfct, q, p, transp='t')
+  qrm_solve!(spfct, q, p, transp = 't')
 end
